b3f1d54f57ab89741ddcd249941ca5db17626ec5
vascarpenter <gikoha5666@gmail.com>
2021-02-04 11:07:10 +0900

    argument bug fix

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
-// JapMedStatKt <filename_SJIS.txt>
+// java -jar JapMedStatKt.jar <filename_SJIS.txt>
 
 import java.io.File
 import java.nio.charset.Charset
@@ -32,17 +32,16 @@ fun main(args: Array<String>) {
     var abstr: String = ""
 
     try {
-        var file: File;
-        if( args.size<2)
+        if(args.size==0)
         {
-            file = File("/tmp/test.txt")
-        } else {
-            file = File(args[1])
+            System.err.println("usage: java -jar JapMedStatKt.jar <filename_sjis.txt>")
+            System.exit(1)
         }
+        var file: File  = File(args[0])
         val bufferedReader = file.bufferedReader(Charset.forName("MS932"))
 
         // parse OCISTRING env variable and use IP/userid/pass
-        var ocistring = System.getenv("OCISTRING") ?: "user/pass@localhost/XEPDB1"
+        var ocistring = System.getenv("OCISTRING") ?: "user/pass@//localhost/XEPDB1"
         val match = Regex("""^([a-zA-Z0-9]+)/([a-zA-Z0-9]+)@(.*)$""").find(ocistring)
         var jdbcuser = match!!.groups[1]!!.value
         var jdbcpass = match!!.groups[2]!!.value
